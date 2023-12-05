22dac466d5888bde2cbab2e03f00a2c5cc6f49d5
Daniël <dahnjohl@gmail.com>
2023-12-05 21:37:46 +0100

    this works

--- 1/a.py
+++ 1/a.py
@@ -1,7 +1,7 @@
 def main():
     numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     all_numbers = []
-    input = open('input.txt', 'r')
+    input = open('a.txt', 'r')
     lines = input.readlines()
     for line in lines:
         ltr = 0;
